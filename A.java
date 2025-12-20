eebe4ec340122c5048367e2e1fa655f279a189e8
Disha Agarwalla <dishaagarwalla260@gmail.com>
2025-12-20 22:29:50 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class A
+{
+public static void main(String[] args)
+{
+int num, i,j;
+ Scanner sc= new Scanner(System.in); 
+    
+ System.out.print("Enter a Number:");
+ num = sc.nextInt();   
+   
+for(i=1; i<100; i++)
+{
+ for(j=2;j<i;i++)
+{
+ if(i%j != 0)
+{
+System.out.println(i);
+break;
+}
+
+}
+
+}      
+
+}
+}

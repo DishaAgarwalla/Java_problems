c601825bc741737cebe15190ec5c5b4583492429
Disha Agarwalla <dishaagarwalla260@gmail.com>
2025-12-20 22:31:41 +0530

    Add files via upload

---
+++ CD.java
@@ -0,0 +1,20 @@
+class AB
+{
+void xyz()
+{
+System.out.println("hi");
+}
+void disp()
+{
+this.xyz();
+System.out.println("hello");
+}
+}
+ class CD
+{
+public static void main(String [] args)
+{
+AB p= new AB();
+p.disp();
+}
+}

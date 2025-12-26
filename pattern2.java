cff922d90762882573bc9342bb6b98268a50d7c0
Disha Agarwalla <dishaagarwalla260@gmail.com>
2025-12-26 18:05:08 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,16 @@
+class dd 
+{
+public static void main(String args[]){
+	int n =1;
+	int i,j;
+for (i=1; i<=3;i++)
+{
+for ( j =1; j<=5;j++)
+{
+System.out.print(n);
+	n++;
+}
+System.out.println();
+}
+}
+}

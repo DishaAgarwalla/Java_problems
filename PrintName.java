20dedaa5e398d987ca5555bd750efdd3f958c4f4
Disha Agarwalla <dishaagarwalla260@gmail.com>
2025-12-30 19:32:10 +0530

    Add files via upload

---
+++ PrintName.java
@@ -0,0 +1,19 @@
+
+package mypackage;
+
+public class Myclass {
+    public void getNames(String s) {
+        System.out.println(s);
+    }
+}
+
+
+import mypackage.Myclass;
+
+public class printName {]'
+    public static void main(String[] args) {
+        String name = "welcome"; 
+        Myclass obj = new Myclass();
+        obj.getNames(name); 
+    }
+}

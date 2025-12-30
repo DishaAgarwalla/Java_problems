e9f75af2288fc7282212a3199df4a9ec31ef7cb2
Disha Agarwalla <dishaagarwalla260@gmail.com>
2025-12-30 19:30:03 +0530

    Add files via upload

---
+++ PQ.java
@@ -0,0 +1,23 @@
+class xyz
+{
+protected  int x,y;
+void getdata()
+{
+x=50;
+y=30;
+}
+void show()
+{
+System.out.println(x+" "+y);
+}
+}
+class PQ
+{
+public static void main(String []args)
+{
+xyz p=new xyz();
+p.getdata();
+p.show();
+System.out.println(p.x);
+}
+}

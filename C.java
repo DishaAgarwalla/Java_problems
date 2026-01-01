8cbf0b9b9fc3270efc3c60614074617432db0b22
Disha Agarwalla <dishaagarwalla260@gmail.com>
2026-01-01 20:02:47 +0530

    Add files via upload

---
+++ C.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class C {
+
+public static void main(String[] args) {
+
+ int count = 0, num = 7890;
+
+ for (; num != 0; num /= 10, ++count) {
+}
+
+ System.out.println("Number of digits: " + count);
+  }
+}

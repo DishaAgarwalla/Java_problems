66e20dc06c7ea0e0f09b9aed05195ea060bd0d25
Disha Agarwalla <dishaagarwalla260@gmail.com>
2026-01-02 18:18:05 +0530

    Add files via upload

---
+++ Copy.java
@@ -0,0 +1,31 @@
+class Xyz
+{
+int n;
+Xyz()
+{
+n=0;
+}
+Xyz(int x)
+{
+n=x;
+}
+Xyz(Xyz p)
+{
+n=p.n;
+Void show()
+{
+System.out.println(n);
+}
+}
+class pqr
+{
+public static void main(String []args)
+{
+Xyz k=new Xyz();
+Xyz m=new Xyz(100);
+Xyz z=new Xyz(m);
+ m=show();
+ z=show();
+}
+}
+}

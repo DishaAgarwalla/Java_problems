514b1a525f924a5e00c4b885d4e53430a3b23600
Disha Agarwalla <dishaagarwalla260@gmail.com>
2026-01-05 09:57:37 +0530

    Add files via upload

---
+++ E1.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+class E1
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a intiger number");
+		int a = sc.nextInt();
+		System.out.println("ENter a floting number");
+		float b = sc.nextFloat();
+		System.out.println("Enter a double number");
+		double c = sc.nextDouble();
+		System.out.println("Enter a boolean number ");
+		boolean d = sc.nextBoolean();
+		System.out.println("Enter a string ");
+		String e = sc.next();
+		
+
+	}
+}

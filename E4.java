ea85053785c464599cb4f148ecab7636f3a3b964
Disha Agarwalla <dishaagarwalla260@gmail.com>
2026-01-06 09:52:39 +0530

    Add files via upload

---
+++ E4.java
@@ -0,0 +1,15 @@
+
+class E4
+{
+	public static void main(String [] args)
+	{
+		int x, y, sum ;
+		
+		
+		x =Integer.parseInt(args[0]);
+		y =Integer.parseInt(args[1]);
+		multi  = x*y;
+
+		System.out.println(" multiplication of two numbers" +multi);
+	}
+}

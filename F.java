20ce0f592980041575b854e8e04b60c1d38431a6
Disha Agarwalla <dishaagarwalla260@gmail.com>
2026-01-07 09:45:20 +0530

    Add files via upload

---
+++ F.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class F
+{
+public double far(double c)
+{
+return (c-32)*5/9;
+}
+}
+class B2
+{ 
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the celcius ");
+double v = sc.nextDouble();
+F b1 = new F();
+double d = b1.far(v);
+System.out.println("celcius to farenhite"+d);
+}
+}
